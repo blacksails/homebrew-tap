2e1293334696c608d82ef34266d57bfb438968a7
Oscar A. B. Orellana <oscar@nuntio.io>
2023-01-29 11:41:30 +0100

    Brew formula update for cli version v0.0.54

--- nuntio.rb
+++ nuntio.rb
@@ -5,20 +5,20 @@
 class Nuntio < Formula
   desc ""
   homepage "https://github.com/nuntiodev/cli/nuntio"
-  version "0.0.53"
+  version "0.0.54"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nuntiodev/cli/releases/download/v0.0.53/cli_0.0.53_darwin_arm64.tar.gz"
-      sha256 "269c82b951c2435fe5058f87000b643ac1c2e6a46142adf7301e3db6188a7d8f"
+      url "https://github.com/nuntiodev/cli/releases/download/v0.0.54/cli_0.0.54_darwin_arm64.tar.gz"
+      sha256 "57b0154cf5353fcc2c57abb49b33bcbd45457bbd3e17fce9a604659d9aa1dc7f"
 
       def install
         bin.install "nuntio"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/nuntiodev/cli/releases/download/v0.0.53/cli_0.0.53_darwin_amd64.tar.gz"
-      sha256 "1f5b2c53f591e729de6d8e3031a9e10ae400e7d101399b1d6407eead8b1185fb"
+      url "https://github.com/nuntiodev/cli/releases/download/v0.0.54/cli_0.0.54_darwin_amd64.tar.gz"
+      sha256 "cf547217a2034b1d5cfabd9feaf6f97ab2d687052ec4212e66d4518a59897f80"
 
       def install
         bin.install "nuntio"
@@ -27,17 +27,17 @@ class Nuntio < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/nuntiodev/cli/releases/download/v0.0.53/cli_0.0.53_linux_arm64.tar.gz"
-      sha256 "04f98628997055cfdee3bbd5bac193e04fe8c55b174f4f0bd408cdc10ddf7339"
+    if Hardware::CPU.intel?
+      url "https://github.com/nuntiodev/cli/releases/download/v0.0.54/cli_0.0.54_linux_amd64.tar.gz"
+      sha256 "cde44822983c17d3207a81850a2624c511400f13c16af15afb3c82cbfed6baa2"
 
       def install
         bin.install "nuntio"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/nuntiodev/cli/releases/download/v0.0.53/cli_0.0.53_linux_amd64.tar.gz"
-      sha256 "57aeae26857a8abb65f85fc9a1e1de888358a5a7a2342da1ecbc68f53ea3ea6e"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/nuntiodev/cli/releases/download/v0.0.54/cli_0.0.54_linux_arm64.tar.gz"
+      sha256 "73d4717e8d9c149ef4df7cf0934c234161f2f742f0c3a0437285110e16701609"
 
       def install
         bin.install "nuntio"
